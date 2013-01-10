24d5948a07ce35596b22aa83472a7e48811d0148
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-01-10 21:09:45 +0900

    不要なmigrateファイルを削除

--- db/migrate/20130108050822_add_mail_id_to_predefined_positions.rb
+++
@@ -1,5 +0,0 @@
-class AddMailIdToPredefinedPositions < ActiveRecord::Migration
-  def change
-    add_column :predefined_positions, :mail_id, :string
-  end
-end
--- db/migrate/20130109073829_add_predefined_position_id_to_staffs.rb
+++
@@ -1,5 +0,0 @@
-class AddPredefinedPositionIdToStaffs < ActiveRecord::Migration
-  def change
-    add_column :staffs, :predefined_position_id, :integer
-  end
-end
