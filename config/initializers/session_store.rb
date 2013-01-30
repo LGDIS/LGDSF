3fcacf0e157f937af4da2f58d86739a5dbd87ac5
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-01-30 21:30:03 +0900

    dalii_store対応

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-Lgdsf::Application.config.session_store :cookie_store, key: '_redmine_session'
+require 'action_dispatch/middleware/session/dalli_store'
+Lgdsf::Application.config.session_store :cookie_store, key: '_LGDSF_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
