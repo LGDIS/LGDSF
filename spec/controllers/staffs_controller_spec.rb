7d31a621dbf3bd0a8e5985aed8f2b37223f04b71
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-02-04 12:25:20 +0900

    Specファイルstaffs_controller_spec.rbを修正

--- spec/controllers/staffs_controller_spec.rb
+++ spec/controllers/staffs_controller_spec.rb
@@ -2,6 +2,12 @@
 require 'spec_helper'
 
 describe StaffsController do
+
+  before 'ApplicationController' do
+    @gathering_positions  = Rails.cache.read("gathering_position")
+    @predefined_positions = Rails.cache.read("predefined_position")
+  end
+
   describe 'layout_selector' do
   end
 
@@ -46,36 +52,30 @@ describe StaffsController do
               request.mobile?.should be_true
             end
             after '' do
-              #pending("renderが行えないため保留") do
-              #  render "send_form_mobile"
+              #pending 'renderが行えないため保留' do
+              #render "send_form_mobile"
               #end
             end
           end
         end
-        context 'false or nil' do
-          #describe '' do
-          #end
-        end
-        describe 'PC' do
-          it 'FireFoxのとき、nilであること' do
-            controller.request#.env['HTTP_USER_AGENT'] = "Mozilla"
-            request.mobile?.should be_nil
+        context 'false' do
+          describe 'スマートフォン' do
+            it 'iPhoneのとき、falseであること' do
+              controller.request.env['HTTP_USER_AGENT'] = 'iPhone'
+              request.mobile?.should be_false
+            end
+            it 'Androidのとき、falseであること' do
+              controller.request.env['HTTP_USER_AGENT'] = 'Android'
+              request.mobile?.should be_false
+            end
           end
         end
-        describe 'モバイル' do
-          it 'DoCoMoのとき、trueであること' do
-            controller.request.env['HTTP_USER_AGENT'] = 'DoCoMo'
-            request.mobile?.should be_true
-          end
-        end
-        describe 'スマートフォン' do
-          it 'iPhoneのとき、trueであること' do
-            controller.request.env['HTTP_USER_AGENT'] = 'iPhone'
-            request.mobile?.should be_false
-          end
-          it 'Androidのとき、trueであること' do
-            controller.request.env['HTTP_USER_AGENT'] = 'Android'
-            request.mobile?.should be_false
+        context 'nil' do
+          describe 'PC' do
+            it 'FireFoxのとき、nilであること' do
+              controller.request.env['HTTP_USER_AGENT'] = "Mozilla"
+              request.mobile?.should be_nil
+            end
           end
         end
       end
@@ -103,23 +103,50 @@ describe StaffsController do
   end
 
   describe 'index' do
-    before '職員位置確認画面にアクセスする' do
-      pending 'get できないため保留' do
-        get :index
-      end
-      settings = YAML.load_file("#{Rails.root}/config/settings.yml")
-    end
     context '正常の場合' do
-      describe '@latitude（緯度）' do
-        it 'Stringクラスであること' do
+      before '職員位置確認画面にアクセスする' do
+        #pending 'get できないため保留' do
+        #get :index
+        #end
+      end
+      describe '職員位置確認画面のマップの中心' do
+        settings = YAML.load_file("#{Rails.root}/config/settings.yml")
+        it '@latitude（緯度）がStringクラスであること' do
           @latitude  = settings["ldgsf"][Rails.env]["latitude"]
-          @latitude.should be_an_instance_of(String)
+          @latitude.should be_an_instance_of(Float)
+        end
+        it '@longitude（経度）がStringクラスであること' do
+          settings = YAML.load_file("#{Rails.root}/config/settings.yml")
+          @longitude = settings["ldgsf"][Rails.env]["longitude"]
+          @longitude.should be_an_instance_of(Float)
+        end
+        context '2点間の距離を求め、ズーム率を決定する。' do
+          it '2点間の距離を求める' do
+            pending "nil can't be coerced into Floatが出る為保留" do
+            @gathering_positions.each do |id, gathering_position|
+              lat = gathering_position["latitude"].to_f - @latitude
+              lng = gathering_position["longitude"].to_f - @longitude
+              count = id.to_i-1
+              diff = Math::sqrt(lat * lat + lng * lng)
+              @zoom = Math::log(534.0/diff)/Math::log(2) < @zoom ? Math::log(534.0/diff)/Math::log(2) : @zoom
+            end
+            end
+          end
+          it 'ズーム率を決定する' do
+            pending '上記結果を使用するため保留' do
+            @zoom = @zoom.round - 1
+            end
+          end
         end
       end
-      describe '@longitude（経度）' do
+      describe '最新メール番号' do
         it 'Stringクラスであること' do
-          @longitude = settings["ldgsf"][Rails.env]["longitude"]
-          @longitude.should be_an_instance_of(String)
+          @new_mail_id = Staff.maximum(:mail_id)
+          @new_mail_id.should be_an_instance_of(String)
+        end
+        it 'Staff DBから値が取得できる' do
+          @staffs = Staff.find(:all, :conditions => { :mail_id => @new_mail_id })
+          @staffs.should be_true
         end
       end
     end
