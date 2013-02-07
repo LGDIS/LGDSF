08d66042920e5992ba1c42fab89eb7ede6eb770c
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-02-07 19:34:34 +0900

    マイグレーションファイル修正

--- db/migrate/20121217090208_create_staffs.rb
+++ db/migrate/20121217090208_create_staffs.rb
@@ -3,13 +3,14 @@ class CreateStaffs < ActiveRecord::Migration
   def change
     create_table :staffs do |t|
       t.integer :id
-      t.string  :name
+      t.string  :name, :limit => 64
       t.integer :agent_id
-      t.string  :destination
+      t.string  :destination_code
       t.boolean :status
       t.text    :reason
       t.decimal :latitude,  :precision => 10, :scale => 6
       t.decimal :longitude, :precision => 10, :scale => 6
+      t.string  :disaster_code
 
       t.timestamps
     end
@@ -17,11 +18,12 @@ class CreateStaffs < ActiveRecord::Migration
     set_column_comment(:staffs, :id,          "ID")
     set_column_comment(:staffs, :name,        "職員名")
     set_column_comment(:staffs, :agent_id,    "職員マスタID")
-    set_column_comment(:staffs, :destination, "参集場所")
+    set_column_comment(:staffs, :destination_code, "参集場所コード")
     set_column_comment(:staffs, :status,      "参集先に向かうのが困難")
     set_column_comment(:staffs, :reason,      "理由")
     set_column_comment(:staffs, :latitude,    "緯度")
     set_column_comment(:staffs, :longitude,   "経度")
+    set_column_comment(:staffs, :disaster_code, "災害番号")
     set_column_comment(:staffs, :created_at,  "作成時刻")
     set_column_comment(:staffs, :updated_at,  "更新時刻")
   end
