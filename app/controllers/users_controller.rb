3413806a1f5591fb11e549a289d11d62602880e3
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-02-08 12:08:34 +0900

    デバッグコメントが残っていたのを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,32 +2,20 @@
 
   # SignOut後にAlertが表示されるのを回避するため
   def after_sign_out_path_for(resource_or_scope)
-p "after_sign_out_path_for"
     new_user_session_path
   end
 
   def index
-p "index"
     @users = User.all
   end
 
   def new
-p "new"
     super
   end
 
   def create
-=begin
-    @user = User.new(params[:user])
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to users_path }
-      else
-        format.html { render new_users_path }
-      end
-    end
-=end
-p "create"
+
+    # ログイン名入力チェック
     if params[:user][:email].blank?
       set_flash_message(:alert, :invalid_login)
       redirect_to(:action => :new)
