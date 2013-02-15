b7e75a76fbb63801dc273d89c565fd9e81eecaf1
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-02-15 11:44:25 +0900

    参集場所報告画面に備考（肩代わり機能）を追加

---
+++ app/models/note.rb
@@ -0,0 +1,18 @@
+﻿class Note < ActiveRecord::Base
+  attr_accessible :note, :staff_id
+  
+  belongs_to :staff
+  
+  # 対象の職員の災害番号取得処理
+  # ==== Args
+  # ==== Return
+  # 職員の災害番号
+  # ==== Raise
+  def disaster_code
+    self.staff.disaster_code
+  end
+  
+  validates :note,
+              :length => {:maximum => 20}
+  
+end
---
+++ db/migrate/20130214030135_create_notes.rb
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+class CreateNotes < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.integer :id
+      t.string  :note, :limit => 20
+      t.integer :staff_id
+      
+      t.timestamps
+    end
+    
+    set_column_comment(:notes, :id,          "ID")
+    set_column_comment(:notes, :note,        "備考")
+    set_column_comment(:notes, :staff_id,    "職員")
+    set_column_comment(:notes, :created_at,  "作成時刻")
+    set_column_comment(:notes, :updated_at,  "更新時刻")
+  end
+end
