de78a0a3c3b2adc022a410dbb79f5cf06928d906
githubyoshihiro <yoshihiro.suzuki@carrot.co.jp>
2013-06-14 16:03:37 +0900

    本番環境でマップが表示されなくなるバグ修正 assets:clean、assets:precompileが必要

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,9 @@ Lgdsf::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( staffs_index.js )
+  config.assets.precompile += %w( smartphone.js )
+  config.assets.precompile += %w( jquery/jquery-ui-1.8.21.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
